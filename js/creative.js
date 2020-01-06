8974609b5ad8bbd88684f110ddb9b01113642921
jsp0269 <jsp0269@naver.com>
2020-01-06 17:35:24 +0900

    Project explain-zero commit

--- js/creative.js
+++ js/creative.js
@@ -39,20 +39,12 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
+  // Modal
+  $('a[href="#pro-allmidifier"]').click(function(event) {
+    event.preventDefault();
 
+    $(this).modal({
+      fadeDuration: 250
+    });
+  });
 })(jQuery); // End of use strict
